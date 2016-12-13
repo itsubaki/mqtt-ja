19bf6f535bc43cab3ce73cb2c35216786c8942ef
tsubaki <tsubaki@moon>
2016-12-13 12:09:52 +0900

    Add Bootstrap

---
+++ setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export GRADLE_HOME=/Users/tsubaki/Dropbox/dev/gradle-2.14
+export PATH=$PATH:$GRADLE_HOME/bin
+
---
+++ src/main/java/com/github/itsubaki/sunflower/Bootstrap.java
@@ -0,0 +1,10 @@
+package com.github.itsubaki.sunflower;
+
+import java.util.Arrays;
+
+public class Bootstrap {
+
+	public static void main(String[] arg) {
+		System.out.println(Arrays.toString(arg));
+	}
+}
